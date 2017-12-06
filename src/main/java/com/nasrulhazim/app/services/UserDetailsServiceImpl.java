5e1ccc5d579a36c5182cfb1a39a5ac031cb37efc
Nasrul Hazim <nasrulhazim.m@gmail.com>
2017-12-06 16:17:11 +0800

    Updated Service on User Details

---
+++ src/main/java/com/nasrulhazim/app/services/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.nasrulhazim.app.services;
+
+import com.nasrulhazim.app.repositories.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import static java.util.Collections.emptyList;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository applicationUserRepository;
+
+    public UserDetailsServiceImpl(UserRepository applicationUserRepository) {
+        this.applicationUserRepository = applicationUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        com.nasrulhazim.app.models.User applicationUser = applicationUserRepository.findFirstByEmail(email);
+        if (applicationUser == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return new User(applicationUser.getEmail(), applicationUser.getPassword(), emptyList());
+    }
+}
